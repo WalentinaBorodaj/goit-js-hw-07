154535e01bc4693b8a717031eab8a54fdf90512d
WalentinaBorodaj <valentinaborodaj4@gmail.com>
2021-12-19 17:44:15 +0200

    task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -65,3 +65,52 @@ galleryEl.insertAdjacentHTML('afterbegin', galleryCreate);
 //     refs.list.onclick = () => { basicLightbox.create(`<img width="1400" height="900" src= "${event.target.dataset.source}">`).show() }
 //   }
 //   }
+
+// ==========================вариант 3 ===========================================
+// import { galleryItems } from './gallery-items.js';
+// // import * as basicLightbox from 'basiclightbox';
+// // Change code below this line
+
+// console.log( galleryItems );
+//   // ===============создаем класс изображений, в который положим все свойства из массива=========
+//   class Gallery1{
+//     constructor(preview, original, description){
+//       this.preview = preview,
+//       this.original = original,
+//       this.description = description
+//     }
+//   }
+//   // ===============получим ссылку родителя галереи======================
+//   const gallery = document.querySelector('.gallery')
+//   // =функция динамической разметки, передаем в нее наш массив изображений и рендерим разметку======
+// const html = (gallery) =>{
+//   return gallery.map( ( img ) => {
+//   const imgEl = new Gallery1 (img.preview, img.original, img.description)
+//   const htmlEl =  `<div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//       <img
+//       class="gallery__image"
+//       src="${imgEl.preview}"
+//       alt="${imgEl.description}"
+//       data-source="${imgEl.original}"
+//       />
+//   </a>
+//   </div>`;
+//   return htmlEl
+// })
+// }
+// // повесим нашу разметку на сайт
+// gallery.insertAdjacentHTML("beforeend", html(galleryItems).join(""))
+
+// gallery.addEventListener( "click", onOriginalClick )
+
+// function onOriginalClick ( e ) {
+//     e.preventDefault();
+//     if ( e.target.nodeName !== 'IMG' ) {
+//         return
+//     }
+// console.log(e.target.dataset.source)
+//     basicLightbox.create(`
+//     <img width="1400" height="900" src="${e.target.dataset.source}">
+//      `).show()
+// }
